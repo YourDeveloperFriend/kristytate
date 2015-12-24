42e6a755a39d6cd66ee6b08927837241ef7a4ac5
Nathan Tate <nathantate88@gmail.com>
2015-12-24 11:57:23 -0700

    Submitting an email

--- src/submit-to-mailchimp.js
+++ src/submit-to-mailchimp.js
@@ -5,8 +5,19 @@
   var $input = document.getElementById('email-yui_3_17_2_22_1440713902689_8357-field');
   
   $form.addEventListener('submit', function(e) {
-    e.preventDefault();
-    alert('submit to mailchimp! ' + $input.value);
+    var email = $input.value;
+    if(email && email.length) {
+      e.preventDefault();
+
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          alert('booya!');
+        }
+      };
+      xhttp.open("POST", "submit-email.php", true);
+      xhttp.send('email=' + encodeURIComponent(email));
+    }
   });
 
 })(document);
