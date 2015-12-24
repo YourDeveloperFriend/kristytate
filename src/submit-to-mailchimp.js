a32e113f7243dfde6b6f355e3b83def63064dd6e
Nathan Tate <nathantate88@gmail.com>
2015-12-24 16:06:32 -0700

    Added proper confirmation

--- src/submit-to-mailchimp.js
+++ src/submit-to-mailchimp.js
@@ -1,18 +1,25 @@
 
 (function(document) {
 
+  var inputId = 'email-yui_3_17_2_22_1440713902689_8357-field';
   var $form = document.getElementById('formform')
-  var $input = document.getElementById('email-yui_3_17_2_22_1440713902689_8357-field');
+  var $confirm = document.getElementById('formform2');
+  var $input = document.getElementById(inputId);
+  var $submitBtn = document.getElementById('email-submit-btn');
   
   $form.addEventListener('submit', function(e) {
     var email = $input.value;
     if(email && email.length) {
       e.preventDefault();
 
+      var prevStyle = $submitBtn.style.display;
+      $submitBtn.style.display = 'none';
       var xhttp = new XMLHttpRequest();
       xhttp.onreadystatechange = function() {
         if (xhttp.readyState == 4 && xhttp.status == 200) {
-          alert('booya!');
+          $submitBtn.style.display = prevStyle;
+          $form.style.display = 'none';
+          $confirm.style.display = 'block';
         }
       };
       xhttp.open("POST", "submit-email.php", true);
