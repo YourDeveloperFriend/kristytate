f91141f05a0a202a2b427c17837d4a52bcdf94bb
Nathan Tate <nathantate@nathantate-macbookpro.roam.corp.google.com>
2017-04-19 16:11:14 -0700

    Migrate from mailchimp to mailerlite

--- submit-email.php
+++ submit-email.php
@@ -1,21 +1,17 @@
 <?php
 
-include dirname(__FILE__).'/mailchimp.php';
-$api = getenv('MAILCHIMP_ID');
-$listid = 'b2e037880a';
+include dirname(__FILE__).'/mailerlite/Base/RestBase.php';
+include dirname(__FILE__).'/mailerlite/Base/Rest.php';
+include dirname(__FILE__).'/mailerlite/Subscribers.php';
+$API_KEY = getenv('MAILERLITE_KEY');
 
-$mailchimp = new \Drewm\MailChimp($api);
+$ML_Subscribers = new MailerLite\Subscribers($API_KEY);
+
 $post = array();
 parse_str(file_get_contents('php://input'), $post);
 
 if(isset($post['email']) && is_string($post['email']) && strlen($post['email']) > 0) {
   $email = $post['email'];
-  $result = $mailchimp->call('lists/subscribe', array(
-    'id' => $listid,
-    'email' => array('email' => $email),
-    'double_optin' => false,
-    'update_existing' => true,
-    'replace_interests' => false,
-    'send_welcome' => false,
-  ));
+  $subscriber = array('email' => $email);
+  $ML_Subscribers->setId($listId)->setAutoresponders(true)->add($subscriber);
 }
