edcfc9910bd3f84a2217ed984ea661f471156c7a
Nathan Tate <nathantate@nathantate-macbookpro.roam.corp.google.com>
2017-04-19 18:11:16 -0700

    Fixed

--- submit-email.php
+++ submit-email.php
@@ -14,21 +14,21 @@ parse_str(file_get_contents('php://input'), $post);
 
 if(isset($post['email']) && is_string($post['email']) && strlen($post['email']) > 0) {
   $email = $post['email'];
-  $field_string = 'email=' . urlencode($email);
+  $field_string = json_encode(array('email' => $email));
 
   $url = "https://api.mailerlite.com/api/v2/groups/$listId/subscribers";
 
-
-  $ch = curl_init();
+  $ch = curl_init($url);
+                                                                   
+  curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+  curl_setopt($ch, CURLOPT_POSTFIELDS, $field_string);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
   curl_setopt($ch, CURLOPT_HTTPHEADER, array(
     'X-MailerLite-ApiKey' => $API_KEY,
-    'Content-Type' => 'application/x-www-form-urlencoded'
+    'Content-Type' => 'application/json',
+    'Content-Length' => strlen($field_string),
   ));
-
-  curl_setopt($ch,CURLOPT_URL,$url);
-  curl_setopt($ch,CURLOPT_POST,1);
-  curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
 
   $result = curl_exec($ch);
   print $result;
