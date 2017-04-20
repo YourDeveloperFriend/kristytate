1a9a795c66fc1395fd38b4d5a55e92e2a121f207
Nathan Tate <nathantate@nathantate-macbookpro.roam.corp.google.com>
2017-04-19 17:27:31 -0700

    New Mailer

--- submit-email.php
+++ submit-email.php
@@ -1,9 +1,11 @@
 <?php
 
-include dirname(__FILE__).'/mailerlite/Base/RestBase.php';
-include dirname(__FILE__).'/mailerlite/Base/Rest.php';
-include dirname(__FILE__).'/mailerlite/Subscribers.php';
+include dirname(__FILE__).'/mailerlite/src/Common/ApiConstants.php';
+include dirname(__FILE__).'/mailerlite/src/Common/RestClient.php';
+include dirname(__FILE__).'/mailerlite/src/MailerLite.php';
+
 $API_KEY = getenv('MAILERLITE_KEY');
+$listId = '6065355';
 
 $ML_Subscribers = new MailerLite\Subscribers($API_KEY);
 
@@ -12,6 +14,22 @@ parse_str(file_get_contents('php://input'), $post);
 
 if(isset($post['email']) && is_string($post['email']) && strlen($post['email']) > 0) {
   $email = $post['email'];
-  $subscriber = array('email' => $email);
-  $ML_Subscribers->setId($listId)->setAutoresponders(true)->add($subscriber);
+  $field_string = 'email=' . urlencode($email);
+
+  $url = "https://api.mailerlite.com/api/v2/groups/$listId/subscribers";
+
+
+  $ch = curl_init();
+
+  curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+    'X-MailerLite-ApiKey' => $API_KEY,
+    'Content-Type' => 'application/x-www-form-urlencoded'
+  ));
+
+  curl_setopt($ch,CURLOPT_URL,$url);
+  curl_setopt($ch,CURLOPT_POST,1);
+  curl_setopt($ch,CURLOPT_POSTFIELDS,$fields_string);
+
+  $result = curl_exec($ch);
+  print $result;
 }
